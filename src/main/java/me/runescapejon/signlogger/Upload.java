91feeebaba17536992931b88b995f9d2cf424cc3
runescapejon <jonsimpletv@gmail.com>
2020-04-16 16:22:54 -0400

    Update Upload.java

--- src/main/java/me/runescapejon/signlogger/Upload.java
+++ src/main/java/me/runescapejon/signlogger/Upload.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
